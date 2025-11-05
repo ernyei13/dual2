20a8e7c89e38e99869b44703072cfec3a27f4ab2
ernyei13 <bee.logger2@gmail.com>
2025-11-05 22:35:47 +0100

    change

--- src/brachiation_env.py
+++ src/brachiation_env.py
@@ -1,11 +1,3 @@
-0,0 +1,32 @@
-# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES, All rights reserved.
-#
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-#
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
 
 from omni.isaac.lab.envs import ManagerBasedRLEnv
 
